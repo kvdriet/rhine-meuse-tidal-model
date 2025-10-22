3c35f41c4cebc3bc9f2e8d56ed554b15182fef85
krisvdr <43061470+krisvdr@users.noreply.github.com>
2025-10-22 23:02:37 +0200

    Update import to network_model_RM

--- Backend/main.py
+++ Backend/main.py
@@ -6,7 +6,7 @@ import numpy as np
 import json
 
 # Import your model class
-from rhine_meuse_test import Network_model_RM
+from Network_model_RM import Network_model_RM
 
 app = FastAPI(title="Rhine-Meuse Tidal Model API")
 
